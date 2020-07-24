70f6b508198f05c767e918fed83b3d0c6fe12004
rohanmanijohn <68681733+rohanmanijohn@users.noreply.github.com>
2020-07-24 20:30:47 +0530

    Add files via upload

---
+++ git demo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 18:54:20 2020
+
+@author: rohan.john
+"""
+
+list1=["Mr Issac Newton", "Mr Nicola Tesla"]
+list2=[]
+empty=[" "]
+
+for i in range (0,len(list1)):
+    a=list1[i].split()
+    k=a[0]+ empty[0] + a[-1]
+    list2.append(k)
+    
+print(list2)         
